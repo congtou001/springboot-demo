0c48387a1ed773fde4566e91ac494538aff67e02
congtou001 <yangdongxuan@163.com>
2022-01-14 10:01:32 +0800

    Add lombok annotation to GreetingModel

--- src/main/java/com/congtou/demo/model/GreetingModel.java
+++ src/main/java/com/congtou/demo/model/GreetingModel.java
@@ -1,20 +1,12 @@
 package com.congtou.demo.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class GreetingModel {
 
     private final long id;
     private final String content;
-
-    public GreetingModel(long id, String content) {
-        this.id = id;
-        this.content = content;
-    }
-    
-	public long getId() {
-		return id;
-	}
-
-	public String getContent() {
-		return content;
-	}
 }
